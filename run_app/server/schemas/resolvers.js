10fb1d540a8c9b5256357fd49ed0d64353f1d7dc
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-02-13 13:12:25 -0500

    added add user mutation

--- run_app/server/schemas/resolvers.js
+++ run_app/server/schemas/resolvers.js
@@ -29,5 +29,13 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        }
     },
 };
+
+module.exports = resolvers;
