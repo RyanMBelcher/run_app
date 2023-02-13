ae8c55cb2647f0e00320ebfd4296fdf8e09c41ea
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-02-13 13:09:09 -0500

    added login mutation and get user query

---
+++ run_app/server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Goal } = require('..models');
+cosnt { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // Get logged in user information
+        me: async (parent, args, context) => {
+            if (context.user || args.user) {
+                const user = await User.findOne({ username: args.username || context.user.username }).populate('goals');
+                return user;
+            }
+            throw new AuthenticationError('You must be logged in!');
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+    },
+};
