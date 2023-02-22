2edbb2e5efe1d13c82aacc1c58c49f50ac128395
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-02-22 16:42:29 -0500

    added pseudo code to goals model

--- run_app/server/models/Goal.js
+++ run_app/server/models/Goal.js
@@ -2,9 +2,21 @@ const { Schema, model } = require('mongoose');
 
 const goalSchema = new Schema(
     {
-
+        // goal title
+        // connection to the map?
+        // post ref Post
+        // username
+    }
+    {
+        toJSON: {
+            virtuals: true,
+        }
     }
 );
+
+goalSchema.virtual('postCount').get(function () {
+    return this.posts.length;
+})
 
 const Goal = model('Goal', goalSchema);
 
