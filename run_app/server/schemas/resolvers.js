74c3904995afdebf6a80f81290515017a70b0349
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-02-23 11:29:12 -0500

    added follower, comment, like mutations

--- run_app/server/schemas/resolvers.js
+++ run_app/server/schemas/resolvers.js
@@ -12,16 +12,6 @@ const resolvers = {
             }
             throw new AuthenticationError('You must be logged in!');
         },
-        // getSingleUser
-        // getAllUsers
-        // getGoalByUser
-        // getSingleGoal
-        // getAllPosts
-        // getPostByUser
-        // getPostByGoal
-        // getSinglePost
-        // getComment
-        // getFollowers
         getSingleUser: async (parent, { username }, context) => {
             const user = await User.findOne({ username }).populate('goals').populate('posts').populate('followers');
             return user;
@@ -83,6 +73,115 @@ const resolvers = {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
+        },
+        editProfile: async (parent, args, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { username: args.username || context.user.username },
+                {
+                    $set:
+                    {
+                        bio: args.bio,
+                        profileImage: args.profileImage
+                    }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            );
+            return updatedUser;
+        },
+        addFollower: async (parent, args, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { username: args.userId || context.userId },
+                {
+                    $addToSet: {
+                        followers: args.userId || context.user._id
+                    }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            ).populate('followers');
+            return updatedUser;
+        },
+        removeFollower: async (parent, args, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { username: args.blockUsername },
+                {
+                    $pull: {
+                        followers: args.userId || context.user._id
+                    }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            ).populate('followers');
+            return updatedUser;
+        },
+        // addGoal
+        // deleteGoal
+        // addPost
+        // deletePost
+        // editPost
+        addComment: async (parent, args, context) => {
+            const post = await Post.findOneAndUpdate(
+                { _id: args.postId },
+                {
+                    $addToSet: {
+                        comments: {
+                            text: args.text,
+                            username: args.username || context.user.username,
+                            postId: args.postid
+                        }
+                    }
+                },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            ).populate('comments');
+            return post
+        },
+        deleteComment: async (parent, { commentId, postId }) => {
+            return await Post.findOneAndUpdate(
+                { _id: postId },
+                { $pull: { comments: { _id: commentId } } },
+                { new: true }
+            ).populate('goalId')
+        },
+        likePost: async (parent, args, context) => {
+            const post = await Post.findOne({ _ix: args.postId });
+            if (post.likes.includes(context.user._id)) {
+                return await Post.findOneAndUpdate(
+                    { _id: args.postId },
+                    {
+                        $pull: {
+                            likes: args.userId || context.user._id
+                        }
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                ).populate('likes');
+            } else {
+                return await Post.findOneAndUpdate(
+                    { _id: args.postId },
+                    {
+                        $addToSet: {
+                            likes: args.userId || context.user._id
+                        }
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                ).populate('likes');
+                )
+            }
         }
     },
 };
