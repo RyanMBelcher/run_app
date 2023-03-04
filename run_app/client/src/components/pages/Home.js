5966f4d224c5f2ce495b8e9f5b3d87844fc4bc3d
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-03-04 17:02:27 -0500

    combined navbar and heder components

--- run_app/client/src/components/pages/Home.js
+++ run_app/client/src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../index.css'
 // import Auth from '../../utils/auth';
+import man_running_on_bridge from '../../images/man_running_on_bridge.png';
 
 export default function Home() {
     return (
         <div>
             <h1 className='text-3xl font-bold underline'>Welcome to the App!</h1>
+            <img src={man_running_on_bridge}>
+            </img>
         </div>
     )
 }
