c225f289192d2872d4021da6c4529ef8bbecd397
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-03-04 22:45:48 -0500

    deleted tailwind and added chakra

--- run_app/client/src/components/Login.js
+++ run_app/client/src/components/Login.js
@@ -1,71 +1,101 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import {
+    Input,
+    Container,
+    Button,
+    FormControl,
+    FormLabel,
+    Stack,
+    VStack,
+    Flex,
+    Link,
+    Box
+} from '@chakra-ui/react';
+import { LOGIN_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
-// import { LOGIN_USER } from '../utils/mutations';
-// import Auth from '../utils/auth';
+const LoginForm = () => {
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    // const [validated] = useState(false);
+    // const [showAlert, setShowAlert] = useState(false);
 
-// const LoginForm = () => {
-//     const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-//     // const [validated] = useState(false);
-//     // const [showAlert, setShowAlert] = useState(false);
+    const [login, { error }] = useMutation(LOGIN_USER);
 
-//     const [login, { error }] = useMutation(LOGIN_USER);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
 
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target;
-//         setUserFormData({ ...userFormData, [name]: value });
-//     };
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
 
-//     const handleFormSubmit = async (e) => {
-//         e.preventDefault();
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation();
+        }
 
-//         const form = e.currentTarget;
-//         if (form.checkValidity() === false) {
-//             e.preventDefault()
-//             e.stopPropagation();
-//         }
+        try {
+            const result = await login({ variables: { ...userFormData } });
+            Auth.login(result.data.login.token);
+        } catch (error) {
+            console.error(error);
+            // setShowAlert(true);
+        }
 
-//         try {
-//             const result = await login({ variables: { ...userFormData } });
-//             Auth.login(result.data.login.token);
-//         } catch (error) {
-//             console.error(error);
-//             // setShowAlert(true);
-//         }
+        setUserFormData({
+            email: '',
+            password: '',
+        });
+    }
 
-//         setUserFormData({
-//             email: '',
-//             password: '',
-//         });
-//     }
+    return (
+        <Flex>
+            <VStack>
+                justify={'center'}
+                <Stack
+                    flexDir='column'
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    <Container>
+                        <Box display='flex'>
+                            <form onSubmit={handleFormSubmit}>
+                                <FormControl id='email' isRequired>
+                                    <FormLabel htmlFor='email'>Email</FormLabel>
+                                    <Input
+                                        type='email'
+                                        placeholder='Your email address'
+                                        name='email'
+                                        onChange={handleInputChange}
+                                        value={userFormData.email}
+                                        required
+                                    />
+                                    <FormLabel htmlFor='password'>Password</FormLabel>
+                                    <Input
+                                        type='password'
+                                        placeholder='Your password'
+                                        name='password'
+                                        onChange={handleInputChange}
+                                        value={userFormData.password}
+                                        required
+                                    />
+                                    <Button type='submit'
+                                        // as={ReactLink}
+                                        // to='/'
+                                        size='md'
+                                    >
+                                        Login
+                                    </Button>
+                                </FormControl>
+                            </form>
+                        </Box>
+                    </Container>
+                </Stack>
+            </VStack>
+        </Flex>
+    );
+};
 
-//     return (
-//         <div className='login-form'>
-//             <form onSubmit={handleFormSubmit}>
-//                 <div className='login-input'>
-//                     <label htmlFor='email'>Email</label>
-//                     <input
-//                         type='email'
-//                         placeholder='Your email address'
-//                         name='email'
-//                         onChange={handleInputChange}
-//                         value={userFormData.email}
-//                         required
-//                     />
-//                     <label htmlFor='password'>Password</label>
-//                     <input
-//                         type='password'
-//                         placeholder='Your password'
-//                         name='password'
-//                         onChange={handleInputChange}
-//                         value={userFormData.password}
-//                         required
-//                     />
-//                     <button type='submit' className='signup-login-btn'>Login</button>
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// };
-
-// export default LoginForm;
+export default LoginForm;
--- run_app/client/src/components/Signup.js
+++ run_app/client/src/components/Signup.js
@@ -1,82 +1,82 @@
-// import React, { useState } from 'react';
-// import { useMutation } from '@apollo/client';
-// import Auth from '../utils/auth';
-// import { ADD_USER } from '../utils/mutations';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
 
 
-// const SignupForm = () => {
-//     const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
-//     // const [validated] = useState(false);
-//     // const [showAlert, setShowAlert] = useState(false);
-//     const [addUser, { error }] = useMutation(ADD_USER);
+const SignupForm = () => {
+    const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+    // const [validated] = useState(false);
+    // const [showAlert, setShowAlert] = useState(false);
+    const [addUser, { error }] = useMutation(ADD_USER);
 
-//     const handleInputChange = (e) => {
-//         const { name, value } = e.target;
-//         setUserFormData({ ...userFormData, [name]: value });
-//     };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setUserFormData({ ...userFormData, [name]: value });
+    };
 
-//     const handleFormSubmit = async (e) => {
-//         e.preventDefault();
-//         console.log(userFormData);
-//         const form = e.currentTarget;
-//         if (form.checkValidity() === false) {
-//             e.preventDefault();
-//             e.stopPropagation();
-//         }
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        console.log(userFormData);
+        const form = e.currentTarget;
+        if (form.checkValidity() === false) {
+            e.preventDefault();
+            e.stopPropagation();
+        }
 
-//         try {
-//             const result = await addUser({ variables: { ...userFormData } });
-//             Auth.login(result.data.addUser.token);
-//         } catch (error) {
-//             console.error(error);
-//             // setShowAlert(true);
-//         }
+        try {
+            const result = await addUser({ variables: { ...userFormData } });
+            Auth.login(result.data.addUser.token);
+        } catch (error) {
+            console.error(error);
+            // setShowAlert(true);
+        }
 
-//         setUserFormData({
-//             username: '',
-//             email: '',
-//             password: '',
-//         });
-//     };
+        setUserFormData({
+            username: '',
+            email: '',
+            password: '',
+        });
+    };
 
-//     return (
-//         <div className='login-form'>
-//             <form onSubmit={handleFormSubmit}>
-//                 <div className='login-input'>
-//                     <label htmlFor='username'>Username</label>
-//                     <input
-//                         type='text'
-//                         placeholder='Your Username'
-//                         name='username'
-//                         onChange={handleInputChange}
-//                         value={userFormData.username}
-//                         required
-//                     />
-//                     <label htmlFor='email'>Email</label>
-//                     <input
-//                         type='email'
-//                         placeholder='Your email address'
-//                         name='email'
-//                         onChange={handleInputChange}
-//                         value={userFormData.email}
-//                         required
-//                     />
-//                     <label htmlFor='password'>Password</label>
-//                     <input
-//                         type='password'
-//                         placeholder='Your password'
-//                         name='password'
-//                         onChange={handleInputChange}
-//                         value={userFormData.password}
-//                         required
-//                     />
-//                     <button
-//                         type='submit' className='signup-login-btn'>Signup
-//                     </button>
-//                 </div>
-//             </form>
-//         </div>
-//     );
-// };
+    return (
+        <div className='login-form'>
+            <form onSubmit={handleFormSubmit}>
+                <div className='login-input'>
+                    <label htmlFor='username'>Username</label>
+                    <input
+                        type='text'
+                        placeholder='Your Username'
+                        name='username'
+                        onChange={handleInputChange}
+                        value={userFormData.username}
+                        required
+                    />
+                    <label htmlFor='email'>Email</label>
+                    <input
+                        type='email'
+                        placeholder='Your email address'
+                        name='email'
+                        onChange={handleInputChange}
+                        value={userFormData.email}
+                        required
+                    />
+                    <label htmlFor='password'>Password</label>
+                    <input
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        onChange={handleInputChange}
+                        value={userFormData.password}
+                        required
+                    />
+                    <button
+                        type='submit' className='signup-login-btn'>Signup
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+};
 
-// export default SignupForm;
+export default SignupForm;
