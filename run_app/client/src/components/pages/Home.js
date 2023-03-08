07a50076a39087843d1fa7255b8d8436904106ab
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-03-08 10:58:03 -0500

    switched react versions

--- run_app/client/src/components/pages/Home.js
+++ run_app/client/src/components/pages/Home.js
@@ -1,8 +1,31 @@
 import React, { useState } from 'react';
 import { useQuery, useMutation, useLazyQuery } from '@apollo/client';
-import { GET_ALL_POSTS } from '../../utils/queries';
+import { } from '../../utils/queries';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Box,
+    Text,
+    Flex,
+    Heading,
+    Avatar
+} from '@chakra-ui/react';
+import Auth from '../../utils/auth';
 
 export default function Home() {
 
+    return (
+        <Card maxW='md' maxH='md'>
+            <CardHeader>
+                <Flex spacing='4'>
+                    <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                        <Avatar />
+                        <Heading size='md'>{Auth.getProfile().data.username}</Heading>
+                    </Flex>
+                </Flex>
+            </CardHeader>
+        </Card>
+    )
 
 };
