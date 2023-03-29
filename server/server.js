928e821eaddfff6d63cfc14a3ad5ff90cf01db18
Ryan Belcher <ryanmbelcher86@gmail.com>
2023-03-28 22:06:51 -0400

    adjust server middleware

--- server/server.js
+++ server/server.js
@@ -14,6 +14,9 @@ const server = new ApolloServer({
     context: authMiddleware
 });
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
